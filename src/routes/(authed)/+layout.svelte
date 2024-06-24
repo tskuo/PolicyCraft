f5e51f980d4ffecfae0b340f5adaace042c40a84
Tzu-Sheng Kuo <awinder0230@gmail.com>
2024-06-23 22:59:23 -0400

    Navigation animation

--- src/routes/(authed)/+layout.svelte
+++ src/routes/(authed)/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
 	import Sidebar from '$lib/components/Sidebar.svelte';
+	import { navigating } from '$app/stores';
+	import LoaderCircle from 'lucide-svelte/icons/loader-circle';
 </script>
 
 <div class="sticky top-0 z-50">
@@ -11,6 +13,24 @@
 		<Sidebar></Sidebar>
 	</aside>
 	<main class="sm:col-span-5 m-2">
-		<slot></slot>
+		{#if $navigating}
+			{#if $navigating.to.url.pathname == '/policies'}
+				<div
+					class="sm:w-4/5 h-full flex flex-col space-y-2 justify-center items-center text-muted-foreground"
+				>
+					<p>Loading policies...</p>
+					<LoaderCircle class="w-1/5 h-1/4 animate-spin " />
+				</div>
+			{:else if $navigating.to.url.pathname == '/cases'}
+				<div
+					class="sm:w-4/5 h-full flex flex-col space-y-2 justify-center items-center text-muted-foreground"
+				>
+					<p>Loading cases...</p>
+					<LoaderCircle class="w-1/5 h-1/4 animate-spin " />
+				</div>
+			{/if}
+		{:else}
+			<slot></slot>
+		{/if}
 	</main>
 </div>
