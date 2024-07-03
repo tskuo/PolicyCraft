599646979a8a7bc91db7861980767ca8b720aa44
Tzu-Sheng Kuo <awinder0230@gmail.com>
2024-07-02 22:54:08 -0400

    Add random seeds to prompts

--- src/lib/components/AIAssistantPanel.svelte
+++ src/lib/components/AIAssistantPanel.svelte
@@ -1,24 +1,56 @@
 <script lang="ts">
 	import Button from './ui/button/button.svelte';
+	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
 	import { Skeleton } from '$lib/components/ui/skeleton';
 
 	export let policyDescription;
 
-	let messageHistory: any[] = [];
+	let messageHistory: any[] = [
+		{
+			person: 'AI Assistant',
+			message: `Hi, I am an AI assistant who can help brainstorm cases that illustrate the application of the policy, or cases that reveal the potential flaw of the policy. Please click the buttons below to see some examples.
+`
+		}
+	];
 	let loading = false;
 
-	const messageExample = 'Generate an example case';
-	const messageCounterExample = 'Generate a counterexample case';
-	const promptExample =
-		'In three sentence, provide a short example scenario that follows the following policy: ';
-	const promptCounterExample =
-		'In three sentences, provide a short example scenario that is unclear whether the following policy allows it or not: ';
+	const messageExample = `Cases illustrate the policy`;
+	const messageCounterExample = 'Cases reveal the policy flaws';
+	const promptIllustrativeAllowCase = `You are a helpful assistant focusing on supporting users' reflections on a given policy. In a few sentences, provide an example scenario with a character in the context of a university classroom where the character abides by the following policy: `;
+	const promptIllustrativeDisallowCase = `You are a helpful assistant focusing on supporting users' reflections on a given policy. In a few sentences, provide an example scenario with a character in the context of a university classroom where the character violates the following policy: `;
+	const promptFlawAllowCase = `You are a helpful assistant focusing on supporting users' reflections on a given policy. In a few sentences, provide an example scenario with a character in the context of a university classroom where the character abides by the following policy, but most people might think it should not be allowed: `;
+	const promptFlawDisallowCase = `You are a helpful assistant focusing on supporting users' reflections on a given policy. In a few sentences, provide an example scenario with a character in the context of a university classroom where the character violates the following policy, but most people might think it should be allowed: `;
+	const promptFlawUnclearCase = `You are a helpful assistant focusing on supporting users' reflections on a given policy. In a few sentences, provide an example scenario with a character in the context of a university classroom where it is unclear whether the character violates the following policy: `;
 
 	const generateCase = async (message: string, category: string) => {
 		messageHistory = [...messageHistory, { person: 'You', message: message }];
 		loading = true;
 
-		const prompt = category == 'example' ? promptExample : promptCounterExample;
+		let prompt: string = '';
+		let label: string = '';
+		if (category == 'illustrative') {
+			let i = Math.floor(Math.random() * 2); // i = 0 or 1
+			if (i == 0) {
+				prompt = promptIllustrativeAllowCase;
+				label = `The policy may allow the following case: `;
+			} else {
+				prompt = promptIllustrativeDisallowCase;
+				label = `The policy may disallow the following case: `;
+			}
+		} else if (category == 'flaw') {
+			let i = Math.floor(Math.random() * 3); // i = 0, 1, 2
+			if (i == 0) {
+				prompt = promptFlawAllowCase;
+				label = `The policy may allow the following case but most people might think it should not be allowed: `;
+			} else if (i == 1) {
+				prompt = promptFlawDisallowCase;
+				label = `The policy may disallow the following case but most people might think it should be allowed: `;
+			} else {
+				prompt = promptFlawUnclearCase;
+				label = `The policy may be unclear in the following case: `;
+			}
+		}
+
 		const res = await fetch('/api/assistant/case', {
 			method: 'POST',
 			body: JSON.stringify({
@@ -27,7 +59,8 @@
 		});
 		if (res.ok) {
 			const data = await res.json();
-			messageHistory = [...messageHistory, { person: 'AI Assistant', message: data.text }];
+
+			messageHistory = [...messageHistory, { person: 'AI Assistant', message: label + data.text }];
 			loading = false;
 		} else {
 			messageHistory = [
@@ -42,32 +75,36 @@
 	};
 </script>
 
-<h3 class="font-bold text-lg mt-2">AI Assistant</h3>
-<div class="text-sm">
-	{#each messageHistory as message}
-		<p class="font-bold py-2">{message.person}</p>
-		<p class=" rounded p-2 mb-2 {message.person == 'You' ? 'bg-sky-100' : 'bg-gray-100'}">
-			{message.message}
-		</p>
-	{/each}
-	<div hidden={!loading}>
-		<p class="font-bold py-2">AI Assistant</p>
-		<Skeleton class="w-full h-16" />
+<h3 class="font-bold text-lg my-2">Brainstorm with AI</h3>
+<ScrollArea class="h-[85vh] w-full rounded-md border">
+	<div class="text-sm mx-3">
+		{#each messageHistory as message}
+			<p class="font-bold py-2">{message.person}</p>
+			<p class=" rounded p-2 mb-2 {message.person == 'You' ? 'bg-sky-100' : 'bg-gray-100'}">
+				{message.message}
+			</p>
+		{/each}
+		<div hidden={!loading}>
+			<p class="font-bold py-2">AI Assistant</p>
+			<Skeleton class="w-full h-16" />
+		</div>
 	</div>
-</div>
-<div class="flex flex-col space-y-2 mt-2">
-	<Button
-		variant="secondary"
-		disabled={loading}
-		on:click={() => generateCase(messageExample, 'example')}
-	>
-		{messageExample}
-	</Button>
-	<Button
-		variant="secondary"
-		disabled={loading}
-		on:click={() => generateCase(messageCounterExample, 'counterexample')}
-	>
-		{messageCounterExample}
-	</Button>
-</div>
+	<div class="flex flex-col space-y-2 my-2">
+		<Button
+			variant="secondary"
+			class="mx-3"
+			disabled={loading}
+			on:click={() => generateCase(messageExample, 'illustrative')}
+		>
+			{messageExample}
+		</Button>
+		<Button
+			variant="secondary"
+			class="mx-3"
+			disabled={loading}
+			on:click={() => generateCase(messageCounterExample, 'flaw')}
+		>
+			{messageCounterExample}
+		</Button>
+	</div>
+</ScrollArea>
