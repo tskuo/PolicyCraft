9f22b180e56d78a1024dd57a169c360dec870cb1
Tzu-Sheng Kuo <awinder0230@gmail.com>
2024-07-18 21:18:53 -0400

    Sort and filter case repository

--- src/routes/(authed)/cases/+page.svelte
+++ src/routes/(authed)/cases/+page.svelte
@@ -4,44 +4,117 @@
 	import * as Select from '$lib/components/ui/select';
 	export let data;
 
-	const sortCases = async (value: string) => {
-		console.log('input: ', value);
+	let displayCases = data.cases;
 
-		const newCases = data.cases.sort((a, b) => {
-			const nameA = a.votes.allow.length;
-			const nameB = b.votes.allow.length;
-			if (nameA < nameB) {
-				return -1;
-			}
-			if (nameA > nameB) {
-				return 1;
-			}
+	const sortCases = (sortBy: string) => {
+		if (sortBy == 'title') {
+			displayCases = displayCases.sort((caseA, caseB) => {
+				const a = caseA.title;
+				const b = caseB.title;
+				if (a < b) return -1;
+				else if (a > b) return 1;
+				return 0;
+			});
+		} else if (sortBy == 'new') {
+			displayCases = displayCases
+				.sort((caseA, caseB) => {
+					const a = caseA.createAt;
+					const b = caseB.createAt;
+					if (a < b) return -1;
+					else if (a > b) return 1;
+					return 0;
+				})
+				.reverse();
+		} else if (sortBy == 'allow') {
+			displayCases = displayCases
+				.sort((caseA, caseB) => {
+					const a = caseA.votes.allow.length;
+					const b = caseB.votes.allow.length;
+					if (a < b) return -1;
+					else if (a > b) return 1;
+					return 0;
+				})
+				.reverse();
+		} else if (sortBy == 'disallow') {
+			displayCases = displayCases
+				.sort((caseA, caseB) => {
+					const a = caseA.votes.disallow.length;
+					const b = caseB.votes.disallow.length;
+					if (a < b) return -1;
+					else if (a > b) return 1;
+					return 0;
+				})
+				.reverse();
+		} else if (sortBy == 'unsure') {
+			displayCases = displayCases
+				.sort((caseA, caseB) => {
+					const a = caseA.votes.unsure.length;
+					const b = caseB.votes.unsure.length;
+					if (a < b) return -1;
+					else if (a > b) return 1;
+					return 0;
+				})
+				.reverse();
+		}
+	};
 
-			return 0;
-		});
-
-		data.cases = newCases;
+	const filterCases = (filterBy: string) => {
+		if (filterBy == 'all') {
+			displayCases = data.cases;
+		} else if (filterBy == 'voted') {
+			displayCases = data.cases.filter(
+				(c) =>
+					c.votes.allow.includes(data.user?.userId) ||
+					c.votes.disallow.includes(data.user?.userId) ||
+					c.votes.unsure.includes(data.user?.userId)
+			);
+		} else if (filterBy == 'notvoted') {
+			displayCases = data.cases.filter(
+				(c) =>
+					!c.votes.allow.includes(data.user?.userId) &&
+					!c.votes.disallow.includes(data.user?.userId) &&
+					!c.votes.unsure.includes(data.user?.userId)
+			);
+		}
 	};
 </script>
 
 <div class="grid md:grid-cols-4">
 	<div class="md:col-span-3 p-2">
 		<div class="flex items-center">
-			<p class="hidden md:inline mr-2">Sort to find:</p>
+			<p class="hidden md:inline mr-2">Filter by:</p>
 			<div class="mr-2">
-				<Select.Root>
-					<Select.Trigger class="w-[180px]">
-						<Select.Value placeholder="New cases" />
+				<Select.Root
+					onSelectedChange={(v) => {
+						filterCases(v.value);
+					}}
+				>
+					<Select.Trigger class="w-[160px]">
+						<Select.Value placeholder="all cases" />
 					</Select.Trigger>
 					<Select.Content>
-						<Select.Item
-							value="light"
-							on:click={() => {
-								sortCases('here');
-							}}>Light</Select.Item
-						>
-						<Select.Item value="dark">Dark</Select.Item>
-						<Select.Item value="system">System</Select.Item>
+						<Select.Item value="all">all cases</Select.Item>
+						<Select.Item value="voted">voted</Select.Item>
+						<Select.Item value="notvoted">not voted</Select.Item>
+					</Select.Content>
+				</Select.Root>
+			</div>
+			<p class="hidden md:inline mx-2">Sort by:</p>
+			<div class="mr-2">
+				<Select.Root
+					onSelectedChange={(v) => {
+						sortCases(v.value);
+					}}
+				>
+					<Select.Trigger class="w-[160px]">
+						<Select.Value placeholder="new" />
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Item value="new">new</Select.Item>
+						<Select.Item value="title">title</Select.Item>
+						<Select.Item value="allow">allow votes</Select.Item>
+						<Select.Item value="disallow">disallow votes</Select.Item>
+						<Select.Item value="unsure">unsure votes</Select.Item>
 					</Select.Content>
 				</Select.Root>
 			</div>
@@ -49,9 +122,15 @@
 		<Separator class="my-4" />
 		<div class="grid grid-cols-1 lg:grid-cols-3 gap-1 auto-rows-fr">
 			{#if data.cases.length > 0}
-				{#each data.cases as c (c.id)}
-					<CaseCard {...c} userId={data.user?.userId} userCounts={data.userCounts} />
-				{/each}
+				{#key displayCases}
+					{#if displayCases.length > 0}
+						{#each displayCases as c (c.id)}
+							<CaseCard {...c} userId={data.user?.userId} userCounts={data.userCounts} />
+						{/each}
+					{:else}
+						<p>There are no cases that meet the filtering criteria.</p>
+					{/if}
+				{/key}
 			{:else}
 				<p>The case repository is empty.</p>
 			{/if}
